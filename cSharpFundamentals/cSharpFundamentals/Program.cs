6e9bfd87911f8464ea011d7cee11ca9757d42f7d
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-20 18:36:19 -0500

    starting classes

---
+++ cSharpFundamentals/cSharpFundamentals/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpFundamentals
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chris = new Person();
+
+            chris.FirstName = "Chris";
+            chris.LastName = "Lamothe";
+            chris.Introduce();
+        }
+    }
+}
