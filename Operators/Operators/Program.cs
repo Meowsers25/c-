6dd2035e8cac34040f1572ed575f9a573b91b0c6
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-20 14:11:38 -0500

    or operator

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -14,7 +14,7 @@ namespace Operators
             var b = 2;
             var c = 3;
 
-            Console.WriteLine(c > b && c == a);
+            Console.WriteLine(c > b | c == a);
         }
     }
 }
