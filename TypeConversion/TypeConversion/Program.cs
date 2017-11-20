c03becbc62e51395ea66b119992217a2ec3e9a1e
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-19 23:56:26 -0500

    convert string to int

--- TypeConversion/TypeConversion/Program.cs
+++ TypeConversion/TypeConversion/Program.cs
@@ -10,9 +10,9 @@ namespace TypeConversion
     {
         static void Main(string[] args)
         {
-            int i = 1000;
-            byte b = (byte)i;
-            Console.WriteLine(b);
+            var number = "1234";
+            int i = Convert.ToInt32(number);
+            Console.WriteLine(number);
         }
     }
 }
