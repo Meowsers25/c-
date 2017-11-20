03aaa5eb317e47eb4fa2fa74c961e0459273dfb8
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-20 18:54:32 -0500

    more static classes

--- cSharpFundamentals/cSharpFundamentals/Program.cs
+++ cSharpFundamentals/cSharpFundamentals/Program.cs
@@ -3,20 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using cSharpFundamentals.Math;
 
 namespace cSharpFundamentals
 {
-    public class Person
-    {
-        public string FirstName;
-        public string LastName;
-
-        public void Introduce()
-        {
-            Console.WriteLine("My name is " + FirstName + " " + LastName);
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
@@ -26,6 +16,10 @@ namespace cSharpFundamentals
             chris.FirstName = "Chris";
             chris.LastName = "Lamothe";
             chris.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
         }
     }
 }
