b35cb92ab43e08ceb87b0823fcd8a3d1e1ab9b75
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-19 21:20:08 -0500

    using var

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -10,11 +10,11 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Chris";
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Chris";
 
             Console.WriteLine(number);
             Console.WriteLine(count);
