03ed1107921c011f9e9c2221009efaf4ff28c441
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-19 21:31:51 -0500

    constants

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -10,8 +10,8 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            const float Pi = 3.14f;
+            Console.WriteLine(Pi);
         }
     }
 }
