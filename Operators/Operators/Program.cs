a1ee0fff41f3fbe79ad75a5ba045b29d578001f6
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-20 13:02:32 -0500

    operators addition

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 20;
+            Console.WriteLine(a + b);
+        }
+    }
+}
