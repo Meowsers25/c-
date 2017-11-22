1d1b32476f7fec9d8c66d0a6b7fd564e7658537a
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-21 20:39:14 -0500

    program to display larger number

---
+++ Display Larger Number/Display Larger Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Display_Larger_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            var num1 = Console.ReadLine();
+            var newNum1 = Convert.ToInt32(num1);
+            Console.WriteLine("Enter another number");
+            var num2 = Console.ReadLine();
+            var newNum2 = Convert.ToInt32(num2);
+
+
+            //Console.WriteLine(newNum1 + newNum2);
+            if (newNum1 > newNum2)
+            {
+                Console.WriteLine(newNum1 + " is larger.");
+            }
+            else
+            {
+                Console.WriteLine(newNum2 + " is larger.");
+            }
+        }
+    }
+}
