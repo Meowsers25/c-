e1ed4e468db3896438fd303df5f2e73858111a8a
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-21 19:45:03 -0500

    Exercise 1/section 5

---
+++ ValidateNumber/ValidateNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidateNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 1 and 10");
+            var number = Console.ReadLine();
+            var newNum = Convert.ToInt32(number);
+            //Console.WriteLine(number);
+            if (newNum >= 1 && newNum <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
