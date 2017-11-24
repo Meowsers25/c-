6b335b550bfd0437a49a9d5b3ff6e44ac1f23879
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-23 20:09:20 -0500

    Start wit a for loop

---
+++ ForEachLoops/ForEachLoops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEachLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Katie Nichols";
+
+            for (var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+        }
+    }
+}
