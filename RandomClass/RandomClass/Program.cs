3e4078e4c05ae2540f17f9afcd8bde18f0492e90
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-23 22:14:46 -0500

    starting random class

---
+++ RandomClass/RandomClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(random.Next());
+            }
+        }
+    }
+}
