8dfe58cba2feccef75f4c24f32a30f18459cee81
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-24 10:40:01 -0500

    Divisible by 3 app

---
+++ DivisibleBy3/DivisibleBy3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisibleBy3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 100;
+
+            for (var i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sprred limit");
+            var limit = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Speed");
+            var speed = Convert.ToInt32(Console.ReadLine());
+
+            switch (speed)
+            {
+                case 0:
+                    if (speed <= limit)
+                        Console.WriteLine("Ok");
+                    break;
+
+                case 1:
+                    if (speed > limit && speed <= limit + 5)
+                        Console.WriteLine("1");
+                    break;
+                    
+                        
+                    
+                         
+
+            }
+        }
+    }
+}
