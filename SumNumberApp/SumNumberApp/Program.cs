a1281803ab71080baaf30d13be0a5be95e7f4985
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-24 12:49:06 -0500

    Factorial App

---
+++ SumNumberApp/SumNumberApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumNumberApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number to factorialize?");
+            var number = Convert.ToInt32(Console.ReadLine());
+            var newNumber = 1;
+
+            //Console.WriteLine(number);
+
+            for (var i = 1; i <= number; i++)
+            {
+                //newNumber = newNumber * i;
+                newNumber *= i;
+            }
+            Console.WriteLine("{0}! = {1}", number, newNumber);
+        }
+    }
+}
