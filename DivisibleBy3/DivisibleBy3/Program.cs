8bbdcb66c4e0aacbc66f774dad97f6cdf08f5c0a
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-24 14:04:24 -0500

    refactored divisible by 3 app

--- DivisibleBy3/DivisibleBy3/Program.cs
+++ DivisibleBy3/DivisibleBy3/Program.cs
@@ -11,14 +11,16 @@ namespace DivisibleBy3
         static void Main(string[] args)
         {
             var number = 100;
+            var divisible = 0;
 
             for (var i = 1; i <= number; i++)
             {
                 if (i % 3 == 0)
                 {
-                    Console.WriteLine(i);
+                    divisible++;
                 }
             }
+            Console.WriteLine("There are {0} numbers between 0 and 100 that are divisible by 3", divisible);
         }
     }
 }
