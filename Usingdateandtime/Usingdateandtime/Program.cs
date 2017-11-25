17e0078cb289a8745a02a66fc533ed39e61a42d1
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-25 13:46:05 -0500

    date and time

---
+++ Usingdateandtime/Usingdateandtime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usingdateandtime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2017, 11, 25);
+
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+        }
+    }
+}
