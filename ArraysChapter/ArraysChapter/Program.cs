c0c67a1eb1ee487235c4accad0bb4d4cf2344296
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-24 21:17:19 -0500

    array methods

--- ArraysChapter/ArraysChapter/Program.cs
+++ ArraysChapter/ArraysChapter/Program.cs
@@ -27,6 +27,35 @@ namespace ArraysChapter
             {
                 Console.WriteLine(n);
             }
+
+            //Copy()
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+
+            Console.WriteLine("Effect of Copy()");
+            foreach (var n in another)
+            {
+                Console.WriteLine(n);
+            }
+
+            //Sort()
+            Array.Sort(numbers);
+            Console.WriteLine("Effect of Sort()");
+                foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            //Reverse
+            Array.Reverse(numbers);
+            Console.WriteLine("Effect of Reverse()");
+                foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+            
+
+
         }
     }
 }
