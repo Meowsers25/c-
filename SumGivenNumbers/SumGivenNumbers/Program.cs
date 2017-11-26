74f74a855651bc02d09c7b7975870b685c719e2d
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-25 22:31:14 -0500

    sum given numbers app

---
+++ SumGivenNumbers/SumGivenNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumGivenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a number or enter 'ok' to exit");
+                var input = Console.ReadLine();
+
+                if (input == "ok")
+                {
+                    break;
+                }
+                else
+                {
+                    sum += Convert.ToInt32(input);
+                }
+                Console.WriteLine("The sum of your numbers is {0}", sum);
+            }
+
+
+        }
+    }
+}
