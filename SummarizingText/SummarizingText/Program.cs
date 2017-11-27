d237d1495198bef399725b04c8d5f01143c8b7c3
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-27 09:03:07 -0500

    working on app

---
+++ SummarizingText/SummarizingText/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummarizingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really long long long long text";
+            const int maxLength = 20;
+
+            if(sentence.Length < maxLength)
+            {
+                Console.WriteLine(sentence);
+            }
+            else
+            {
+                var words = sentence.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
+
+                foreach(var word in words)
+                {
+                    summaryWords.Add(word);
+
+                    totalCharacters += words.Length + 1;
+                    if (totalCharacters > maxLength)
+                        break;
+                    
+                }
+                var summary = String.Join(" ", summaryWords) + "...";
+                Console.WriteLine(summary);
+            }
+        }
+    }
+}
