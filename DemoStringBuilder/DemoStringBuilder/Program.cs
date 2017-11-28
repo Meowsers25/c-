865a7079c2a8d2419b436a2fbea862fdef3d4983
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-28 14:02:05 -0500

    chained methods to clean up code

--- DemoStringBuilder/DemoStringBuilder/Program.cs
+++ DemoStringBuilder/DemoStringBuilder/Program.cs
@@ -10,16 +10,17 @@ namespace DemoStringBuilder
     {
         static void Main(string[] args)
         {
-            var builder = new StringBuilder();
-            builder.Append('-', 10);
-            builder.AppendLine();
-            builder.Append("Header");
-            builder.AppendLine();
-            builder.Append('-', 10);
-            
-            builder.Replace('-', '+');
-            builder.Remove(0, 10);
-            builder.Insert(0, new string('-', 10));
+            var builder = new StringBuilder("Hello World");
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
 
             Console.WriteLine(builder);
             Console.WriteLine("First char: " + builder[0]);
