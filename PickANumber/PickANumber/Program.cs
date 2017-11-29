55c0a0ccc81080189e6feffb22520b9227b9daf6
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-29 07:29:54 -0500

    Pick a number app

---
+++ PickANumber/PickANumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = new Random().Next(1, 10);
+            //Console.WriteLine(num);
+
+            for (var i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Guess the number");
+                var guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == num)
+                {
+                    Console.WriteLine("You have chosen wisely, Grasshopper");
+                    return;
+                }
+            }
+            Console.WriteLine("You have lost");
+        }
+    }
+}
