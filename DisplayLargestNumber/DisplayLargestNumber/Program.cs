345021b17ba129cef10b0e9f1bb0435e5203e1a1
Chris Lamothe <c.lamothe25@gmail.com>
2017-11-29 07:43:40 -0500

    Largest number app

---
+++ DisplayLargestNumber/DisplayLargestNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayLargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a set of numbers seperated by a comma");
+            var numbers = Console.ReadLine();
+
+            var newNum = numbers.Split(',');
+
+            var maxNum = Convert.ToInt32(newNum[0]);
+            
+            foreach (var num in newNum)
+            {
+                var number = Convert.ToInt32(num);
+                if (number > maxNum)
+                {
+                    maxNum = number;
+                }
+            }
+            Console.WriteLine("The largest number is: {0}", maxNum);
+
+        }
+    }
+}
